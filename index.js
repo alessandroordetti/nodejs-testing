e52d2109662af19ed77c5c8602b71b173eba363d
alessandroordetti <alessandro.ordetti@gmail.com>
2024-08-02 15:32:37 +0200

    Api and templates created

--- index.js
+++ index.js
@@ -2,20 +2,27 @@ const fs = require('node:fs')
 const http = require('node:http')
 const url = require('node:url')
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf8');
+const dataObject = JSON.parse(data);
+
+
+
+
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
-    switch(pathName){
+    switch (pathName) {
         case '/':
             res.end('Welcome to Homepage');
             break;
         case '/api':
-            res.end('Api route');
+            res.writeHead(200, { "Content-type": "application/json" })
+            res.end(data);
             break;
-        default: 
+        default:
             res.writeHead(404, {
-                "Content-type" : "text/html",
-                "Custom-header" : "Sample-header"
+                "Content-type": "text/html",
+                "Custom-header": "Sample-header"
             })
 
             res.end('Route not found');
