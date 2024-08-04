04c60b49d74476d433c0054bcd530dc98ae6eff4
alessandroordetti <alessandro.ordetti@gmail.com>
2024-08-04 10:19:36 +0200

    Nodemon + slugify installed

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 const fs = require('node:fs')
 const http = require('node:http')
 const url = require('node:url')
+const replaceTemplate = require('./modules/replaceTemplate.js')
 
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
 
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-    return output;
-}
 
 //Templates
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf8');
@@ -25,16 +14,15 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf8');
 const dataObject = JSON.parse(data);
 
-
-
-
 const server = http.createServer((req, res) => {
     const request = req.url;
+
+    console.log('Changed');
+    
 
     const parsed = url.parse(request, true)
 
     const {query, pathname} = parsed;
-    console.log(query);
 
     switch (pathname) {
         case ('/' || '/home'):
